3ef160236ca399c403b67bdb6898a28300e51b79
charles <charles.ferreira@aluno.ufabc.edu.br>
2017-05-09 08:39:57 -0300

    Método para verificar se já existe uma preferência salva pelo docente no quadrimestre

--- src/java/facade/PreferenciasFacade.java
+++ src/java/facade/PreferenciasFacade.java
@@ -6,9 +6,13 @@
 package facade;
 
 import controller.HibernateUtil;
+import java.util.List;
 import javax.ejb.Stateless;
 import model.Preferencias;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -22,6 +26,24 @@ public class PreferenciasFacade extends AbstractFacade<Preferencias> {
         super(Preferencias.class);
     }
 
+    //Verifica se já existe algum registro feito pelo docente no quadrimestre
+    public Preferencias verificaPreferencia(Long pessoa_id, int quad){
+        Session session = getSessionFactory().openSession();
+        Criteria criteria = session.createCriteria(Preferencias.class);
+        criteria.add(Restrictions.eq("pessoa_id", pessoa_id));
+        criteria.add(Restrictions.eq("quadrimestre", quad));
+        criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+        
+        List<Preferencias> results = criteria.list();
+        session.close();
+
+        if(results.size()>0){
+            return results.get(0);
+        } else{
+            return null;
+        }
+    }
+    
     @Override
     protected SessionFactory getSessionFactory() {
         return HibernateUtil.getSessionFactory();
