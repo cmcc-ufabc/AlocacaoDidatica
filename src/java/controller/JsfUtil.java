8b186f150144312cb2daf6e1f02caaa8e329afc0
charles <charles.ferreira@aluno.ufabc.edu.br>
2017-05-09 10:52:58 -0300

    Remoção de linhas

--- src/java/controller/JsfUtil.java
+++ src/java/controller/JsfUtil.java
@@ -5,7 +5,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 
-
 public class JsfUtil {
     
    public static SelectItem[] getSelectItems(List<?> entities, boolean selectOne) {
@@ -37,32 +36,18 @@ public class JsfUtil {
             addErrorMessage(message);
         }
     }
-    
-    
+
     public static void addErrorMessage(String msg) {
-    
         FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg);
         FacesContext.getCurrentInstance().addMessage(null, facesMsg);
-        
     }
-    
-    
 
     public static void addSuccessMessage(String msg) {
-
-//      FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg);
-//      FacesContext.getCurrentInstance().addMessage("sucessInfo", facesMsg);
-        
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "successInfo", msg));
-        
     }
     
     public static void addSuccessMessage(String summary, String detail) {
-
-        
         FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
-        FacesContext.getCurrentInstance().addMessage("sucessInfo", facesMsg);
-        
+        FacesContext.getCurrentInstance().addMessage("sucessInfo", facesMsg); 
     }
-    
 }
